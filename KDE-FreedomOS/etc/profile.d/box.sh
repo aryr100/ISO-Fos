bc0db2a99ba2596349a11a49351a20966d9ebdff
frazzlesgrant1994 <frazer_grant@outlook.com>
2022-08-03 03:05:24 +0100

    updated

---
+++ KDE-FreedomOS/etc/profile.d/box.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+zenity \
+--info \
+--text="FreedomOS is still in <b>Development</b>. \nExpect bugs with the OS or Apps. \nIssues can be logged here. \nhttps://github.com/freedomos/Applications/issues" \
+--title="FreedomOS Message" \
+--ok-label="Continue"
